4248a8961cb959d40a40cf291ef40e7c513d67cb
Scarpato <30941595+faelscarpato@users.noreply.github.com>
2025-12-15 03:45:06 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
@@ -11,3 +12,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
